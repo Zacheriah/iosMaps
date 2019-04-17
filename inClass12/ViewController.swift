2b562db7463c1b9635f60a058b1b67d2a66c4113
Wayman, Zacheriah <zwayman@cci01bgg82aws.local>
2019-04-17 18:55:37 -0400

    Initial Commit

---
+++ inClass12/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  inClass12
+//
+//  Created by Wayman, Zacheriah on 4/17/19.
+//  Copyright © 2019 Wayman, Zacheriah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
